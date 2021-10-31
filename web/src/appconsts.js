61e8eb0f77340f1ca7a3622d29ea81bc709d82a2
Anvar pk <anvar.pk@quest-global.com>
2021-10-31 14:02:42 +0530

    API endpoints added in app consts

--- web/src/appconsts.js
+++ web/src/appconsts.js
@@ -1,4 +1,10 @@
 export const APP_CONSTANTS = {
+    API: {
+        URL: 'http://localhost:3000',
+        ENDPOINTS: {
+            CATEGORIES: '/categories',
+        }
+    },
     ROUTES: {
         PRODUCTS: '/',
         PRODUCT_CREATE: '/product-create',

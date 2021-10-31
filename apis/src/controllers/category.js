a4ae3eb7204c5fe100afaf37d5f0d6042e2c3a26
Anvar pk <anvar.pk@quest-global.com>
2021-10-31 14:49:11 +0530

    categogry controller

--- apis/src/controllers/category.js
+++ apis/src/controllers/category.js
@@ -1,9 +1,10 @@
+const { STATUS_CODES } = require('../appconsts');
 const Category = require('../models/category');
 
 const getCategories = async (req, res) => {
     try {
         const cateogories = await Category.find({});
-        res.send(cateogories);
+        res.status(STATUS_CODES.SUCESS).send(cateogories);
     } catch (error) {
         console.log(error)
         res.status(500).send(error);
